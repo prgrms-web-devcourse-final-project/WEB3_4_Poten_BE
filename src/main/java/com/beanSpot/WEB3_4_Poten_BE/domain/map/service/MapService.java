128c406c1230875720713333d44aa5ab3796ce2c
kk0506014 <kk0506014@gmail.com>
2025-03-26 01:06:54 +0900

    feature: MapService.java / 지도 api 요청 메서드 작성

--- src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/map/service/MapService.java
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/map/service/MapService.java
@@ -1,7 +1,13 @@
 package com.beanSpot.WEB3_4_Poten_BE.domain.map.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
 
 /**
  * -- 지도 서비스 --
@@ -13,4 +19,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class MapService {
 
+    @Value("${kakao.key}")
+    private String kakaoKey;
+
+    @Value("${kakao.url}")
+    private String kakaoUrl;
+
+    public String getGeocode(String address) {
+        RestTemplate restTemplate = new RestTemplate();
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(kakaoUrl)
+                .queryParam("query", address);
+
+        // 헤더에 카카오 API 키를 추가
+        ResponseEntity<Map> response = restTemplate.exchange(
+                uriBuilder.toUriString(),
+                org.springframework.http.HttpMethod.GET,
+                new org.springframework.http.HttpEntity<>(getHeaders()),
+                Map.class
+        );
+
+        // 응답을 처리하고 반환
+        Map<String, Object> responseBody = response.getBody();
+        return responseBody != null ? responseBody.toString() : "No data";
+    }
+
+    private org.springframework.http.HttpHeaders getHeaders() {
+        org.springframework.http.HttpHeaders headers = new org.springframework.http.HttpHeaders();
+        headers.set("Authorization", "KakaoAK " + kakaoKey);
+        return headers;
+    }
 }
