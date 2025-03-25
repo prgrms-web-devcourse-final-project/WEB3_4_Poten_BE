423ddc4472db3af5224f25eb0ea5a67593273519
junho3168 <junho3168@naver.com>
2025-03-25 17:17:47 +0900

    refactor - 안쓰는 import 제거, 더미 데이터 관련 코드 제거

--- src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/cafe/controller/CafeController.java
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/cafe/controller/CafeController.java
@@ -2,16 +2,13 @@ package com.beanSpot.WEB3_4_Poten_BE.domain.cafe.controller;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.beanSpot.WEB3_4_Poten_BE.domain.cafe.dto.CafeInfoResponse;
 import com.beanSpot.WEB3_4_Poten_BE.domain.cafe.dto.CafeUpdateRequest;
 import com.beanSpot.WEB3_4_Poten_BE.domain.cafe.entity.Cafe;
@@ -35,12 +32,6 @@ public class CafeController {
 	@GetMapping
 	public List<CafeInfoResponse> getCafeList() {
 		return cafeService.getCafeList();
-	}
-
-	@PostMapping("/dummy")
-	public ResponseEntity<Cafe> createDummyCafe() {
-		Cafe dummyCafe = cafeService.createDummyCafe();
-		return new ResponseEntity<>(dummyCafe, HttpStatus.CREATED);
 	}
 }
 
