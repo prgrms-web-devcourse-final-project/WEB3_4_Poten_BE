0ea1c4cb15605780a7c0d3de3eefa8b6ed08f1e3
kk0506014 <kk0506014@gmail.com>
2025-03-26 01:06:29 +0900

    feature: MapService.java 생성

---
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/map/service/MapService.java
@@ -0,0 +1,16 @@
+package com.beanSpot.WEB3_4_Poten_BE.domain.map.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * -- 지도 서비스 --
+ *
+ * @author -- 김남우 --
+ * @since -- 3월 25일 --
+ */
+@RequiredArgsConstructor
+@Service
+public class MapService {
+
+}
