d3ed3ea8da1433d278e0e6ea685f32af3907c491
junho3168 <junho3168@naver.com>
2025-03-25 18:42:03 +0900

    refactor - entity에서 업데이트 하는 방식으로 변경

--- src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/cafe/entity/Cafe.java
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/cafe/entity/Cafe.java
@@ -2,6 +2,8 @@ package com.beanSpot.WEB3_4_Poten_BE.domain.cafe.entity;
 
 import java.time.LocalDateTime;
 
+import com.beanSpot.WEB3_4_Poten_BE.domain.cafe.dto.req.CafeUpdateReq;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,7 +15,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 
 @Entity
 @Builder
@@ -49,4 +50,25 @@ public class Cafe {
 	private String image;
 
 	private Boolean disabled;
+
+	public void update(CafeUpdateReq request) {
+		if (request.name() != null) {
+			this.name = request.name();
+		}
+		if (request.address() != null) {
+			this.address = request.address();
+		}
+		if (request.phone() != null) {
+			this.phone = request.phone();
+		}
+		if (request.description() != null) {
+			this.description = request.description();
+		}
+		if (request.image() != null) {
+			this.image = request.image();
+		}
+		this.updatedAt = LocalDateTime.now(); // 업데이트 시간 갱신
+	}
 }
+
+
--- src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/cafe/service/CafeService.java
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/cafe/service/CafeService.java
@@ -3,12 +3,15 @@ package com.beanSpot.WEB3_4_Poten_BE.domain.cafe.service;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
+
 import com.beanSpot.WEB3_4_Poten_BE.domain.cafe.dto.res.CafeInfoRes;
 import com.beanSpot.WEB3_4_Poten_BE.domain.cafe.dto.req.CafeUpdateReq;
 import com.beanSpot.WEB3_4_Poten_BE.domain.cafe.entity.Cafe;
 import com.beanSpot.WEB3_4_Poten_BE.domain.cafe.exception.CafeNotFoundException;
 import com.beanSpot.WEB3_4_Poten_BE.domain.cafe.repository.CafeRepository;
+
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
@@ -30,21 +33,8 @@ public class CafeService {
 		Cafe cafe = cafeRepository.findById(id)
 			.orElseThrow(() -> new CafeNotFoundException(id));
 
-		Cafe updatedCafe = Cafe.builder()
-			.cafeId(cafe.getCafeId())
-			.ownerId(cafe.getOwnerId())
-			.name(request.name() != null ? request.name() : cafe.getName())
-			.address(request.address() != null ? request.address() : cafe.getAddress())
-			.phone(request.phone() != null ? request.phone() : cafe.getPhone())
-			.description(request.description() != null ? request.description() : cafe.getDescription())
-			.image(request.image() != null ? request.image() : cafe.getImage())
-			.latitude(cafe.getLatitude())
-			.longitude(cafe.getLongitude())
-			.createdAt(cafe.getCreatedAt())
-			.updatedAt(LocalDateTime.now())
-			.disabled(cafe.getDisabled())
-			.build();
+		cafe.update(request);
 
-		return cafeRepository.save(updatedCafe);
+		return cafeRepository.save(cafe);
 	}
 }
