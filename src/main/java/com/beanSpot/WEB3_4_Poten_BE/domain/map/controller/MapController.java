221eaf60ad05b9d324d4c358274a3c3603d93114
kk0506014 <kk0506014@gmail.com>
2025-03-26 01:05:20 +0900

    feature: MapController.java 생성

---
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/map/controller/MapController.java
@@ -0,0 +1,18 @@
+package com.beanSpot.WEB3_4_Poten_BE.domain.map.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * -- 지도 컨트롤러 --
+ *
+ * @author -- 김남우 --
+ * @since -- 3월 25일 --
+ */
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/map")
+public class MapController {
+
+}
