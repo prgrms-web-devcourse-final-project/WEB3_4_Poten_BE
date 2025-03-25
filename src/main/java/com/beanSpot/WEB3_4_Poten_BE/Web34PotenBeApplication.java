0c900721a5179f4599595af11cfeced11780d976
haeminK <khm65426542@gmail.com>
2025-03-25 10:18:47 +0900

    initial commit

---
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/Web34PotenBeApplication.java
@@ -0,0 +1,13 @@
+package com.beanSpot.WEB3_4_Poten_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web34PotenBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web34PotenBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beanSpot/WEB3_4_Poten_BE/Web34PotenBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beanSpot.WEB3_4_Poten_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web34PotenBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
