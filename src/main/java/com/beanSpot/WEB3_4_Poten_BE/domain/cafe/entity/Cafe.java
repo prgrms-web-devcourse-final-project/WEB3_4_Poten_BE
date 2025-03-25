b603f1b5e78c4ac7be0c4e8f8c8bb83382279574
junho3168 <junho3168@naver.com>
2025-03-25 16:00:32 +0900

    feature - 카페 엔티티 구현

---
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/cafe/entity/Cafe.java
@@ -0,0 +1,52 @@
+package com.beanSpot.WEB3_4_Poten_BE.domain.cafe.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Cafe {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long cafeId;
+
+	@NotEmpty
+	private Long ownerId;
+
+	private String name;
+
+	private String address;
+
+	private Double latitude;
+
+	private Double longitude;
+
+	private String phone;
+
+	@Column(columnDefinition = "TEXT")
+	private String description;
+
+	private LocalDateTime createdAt;
+
+	private LocalDateTime updatedAt;
+
+	private String image;
+
+	private Boolean disabled;
+}
