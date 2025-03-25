e3d15130355965d292a678c4dda5acbf58d93fea
junho3168 <junho3168@naver.com>
2025-03-25 17:17:23 +0900

    refactor - 안쓰는 import 제거

--- src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/cafe/entity/Cafe.java
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/cafe/entity/Cafe.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
