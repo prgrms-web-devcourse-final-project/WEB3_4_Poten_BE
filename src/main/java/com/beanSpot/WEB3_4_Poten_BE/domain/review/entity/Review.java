ed6c1de12c3519b80905c82edd800cee7427b4f3
junho3168 <junho3168@naver.com>
2025-03-26 16:17:37 +0900

    feature - 리뷰 엔티티 구현

---
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/review/entity/Review.java
@@ -0,0 +1,43 @@
+package com.beanSpot.WEB3_4_Poten_BE.domain.review.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "user_id", nullable = false)
+	private Long userId; // 사용자 ID
+
+	@Column(name = "cafe_id", nullable = false)
+	private Long cafeId; // 카페 ID
+
+	@Column(nullable = false)
+	private int rating; // 평점
+
+	@Column(length = 1000) //추후 수정 가능
+	private String comment;
+
+	@Column(name = "created_at", nullable = false)
+	private LocalDateTime createdAt = LocalDateTime.now();
+
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+
+}
