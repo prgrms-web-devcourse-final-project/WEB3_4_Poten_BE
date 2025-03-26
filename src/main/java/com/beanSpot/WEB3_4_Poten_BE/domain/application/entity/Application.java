3249345d5c3af548e030baa22a28a77e0dee411b
junho3168 <junho3168@naver.com>
2025-03-26 12:36:07 +0900

    feature - 카페 신청 엔티티, 상태 변경 메서드와 상태 Enum 클래스 추가

---
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/application/entity/Application.java
@@ -0,0 +1,48 @@
+package com.beanSpot.WEB3_4_Poten_BE.domain.application.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Application {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	// @ManyToOne
+	// @JoinColumn(name = "user_id", nullable = false)
+	// private User user;
+
+	private String name;
+	private String address;
+	private String phone;
+
+	@Enumerated(EnumType.STRING)
+	private Status status = Status.PENDING;
+
+	private LocalDateTime createdAt = LocalDateTime.now();
+
+	public void approve() {
+		this.status = Status.APPROVED;
+	}
+
+	public void reject() {
+		this.status = Status.REJECTED;
+	}
+}
