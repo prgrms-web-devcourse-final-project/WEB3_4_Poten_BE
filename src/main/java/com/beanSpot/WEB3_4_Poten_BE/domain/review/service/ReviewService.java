ff3684202652b6a916cfd94e005a981a63342bcc
junho3168 <junho3168@naver.com>
2025-03-26 17:08:26 +0900

    feature - 리뷰 서비스 구현

---
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/review/service/ReviewService.java
@@ -0,0 +1,57 @@
+package com.beanSpot.WEB3_4_Poten_BE.domain.review.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.beanSpot.WEB3_4_Poten_BE.domain.review.dto.req.ReviewCreateReq;
+import com.beanSpot.WEB3_4_Poten_BE.domain.review.dto.req.ReviewUpdateReq;
+import com.beanSpot.WEB3_4_Poten_BE.domain.review.dto.res.ReviewRes;
+import com.beanSpot.WEB3_4_Poten_BE.domain.review.entity.Review;
+import com.beanSpot.WEB3_4_Poten_BE.domain.review.exception.ReviewNotFoundException;
+import com.beanSpot.WEB3_4_Poten_BE.domain.review.repository.ReviewRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+	private final ReviewRepository reviewRepository;
+
+	public ReviewRes addReview(ReviewCreateReq request) {
+		Review review = Review.builder()
+			.userId(request.userId())
+			.cafeId(request.cafeId())
+			.rating(request.rating())
+			.comment(request.comment())
+			.build();
+
+		reviewRepository.save(review);
+		return ReviewRes.fromEntity(review);
+	}
+
+	public ReviewRes updateReview(Long reviewId, ReviewUpdateReq request) {
+		Review review = reviewRepository.findById(reviewId)
+			.orElseThrow(() -> new ReviewNotFoundException(reviewId));
+		review.updateReview(
+			request.rating(),
+			request.comment());
+
+		reviewRepository.save(review);
+
+		return ReviewRes.fromEntity(review);
+	}
+
+	public void deleteReview(Long reviewId) {
+		reviewRepository.deleteById(reviewId);
+	}
+
+	public List<ReviewRes> getReviewsByCafeId(Long cafeId) {
+		List<Review> reviews = reviewRepository.findByCafeId(cafeId);
+		return reviews.stream()
+			.map(ReviewRes::fromEntity)
+			.collect(Collectors.toList());
+	}
+
+}
