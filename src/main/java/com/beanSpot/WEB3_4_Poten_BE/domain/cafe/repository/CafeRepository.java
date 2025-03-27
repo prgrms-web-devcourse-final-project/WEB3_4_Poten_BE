8c7168407a228271dd45d76f57d771b3f0fc1f52
kk0506014 <kk0506014@gmail.com>
2025-03-27 14:30:10 +0900

    feature: CafeRepository.java / 카페 이름, 주소로 존재 여부를 확인하는 메서드 생성

--- src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/cafe/repository/CafeRepository.java
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/cafe/repository/CafeRepository.java
@@ -1,7 +1,9 @@
 package com.beanSpot.WEB3_4_Poten_BE.domain.cafe.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.beanSpot.WEB3_4_Poten_BE.domain.cafe.entity.Cafe;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CafeRepository extends JpaRepository<Cafe, Long> {
+    // 특정 주소의 카페가 존재하는지 확인
+    boolean existsByNameAndAddress(String name, String address);
 }
