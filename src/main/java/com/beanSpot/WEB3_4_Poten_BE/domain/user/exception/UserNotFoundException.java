38a85f18181c09b8d59dee1a47cb29c4977d8f6d
junho3168 <junho3168@naver.com>
2025-03-31 16:19:59 +0900

    fix - 기존 잘못된 메시지 수정

--- src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/user/exception/UserNotFoundException.java
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/user/exception/UserNotFoundException.java
@@ -2,6 +2,6 @@ package com.beanSpot.WEB3_4_Poten_BE.domain.user.exception;
 
 public class UserNotFoundException extends RuntimeException {
 	public UserNotFoundException(Long id) {
-		super("신청을 찾을 수 없습니다. ID:" + id);
+		super("사용자를 찾을 수 없습니다. ID:" + id);
 	}
 }
