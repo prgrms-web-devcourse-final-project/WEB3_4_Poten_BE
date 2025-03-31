68eb9d2a83a2069671879c5cb65c1c3923955842
kk0506014 <kk0506014@gmail.com>
2025-03-31 17:30:20 +0900

    feature: FavoriteController.java / 즐겨찾기 건트롤러 생성

---
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/favorite/controller/FavoriteController.java
@@ -0,0 +1,10 @@
+package com.beanSpot.WEB3_4_Poten_BE.domain.favorite.controller;
+
+/**
+ * -- 즐겨찾기 컨트롤러 --
+ *
+ * @author -- 김남우 --
+ * @since -- 3월 31일 --
+ */
+public class FavoriteController {
+}
