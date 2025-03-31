155f715ff5a6ed3c338c049278cdc011484ff2f3
kk0506014 <kk0506014@gmail.com>
2025-03-31 17:30:12 +0900

    feature: Favorite.java / 즐겨찾기 엔티티 생성

---
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/favorite/entity/Favorite.java
@@ -0,0 +1,32 @@
+package com.beanSpot.WEB3_4_Poten_BE.domain.favorite.entity;
+
+import com.beanSpot.WEB3_4_Poten_BE.domain.cafe.entity.Cafe;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * -- 즐겨찾기 엔티티 --
+ *
+ * @author -- 김남우 --
+ * @since -- 3월 31일 --
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cafe_id")
+    private Cafe cafe;
+}
