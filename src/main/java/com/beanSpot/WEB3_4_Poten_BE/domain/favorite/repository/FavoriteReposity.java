8b0012b21b3bdf41996bbff04da8e0c5faa1c395
kk0506014 <kk0506014@gmail.com>
2025-04-01 09:42:06 +0900

    feature: FavoriteReposity.java / 즐겨찾기 레포지토리 생성

---
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/favorite/repository/FavoriteReposity.java
@@ -0,0 +1,10 @@
+package com.beanSpot.WEB3_4_Poten_BE.domain.favorite.repository;
+
+/**
+ * -- 즐겨찾기 레포지토리 --
+ *
+ * @author -- 김남우 --
+ * @since -- 3월 31일 --
+ */
+public class FavoriteReposity {
+}
