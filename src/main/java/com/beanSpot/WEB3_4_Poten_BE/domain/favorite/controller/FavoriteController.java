10ba05f6c03f177082f0fbfe2076c4841cb04d03
kk0506014 <kk0506014@gmail.com>
2025-04-01 17:44:50 +0900

    feature: FavoriteController.java / Mapping, Service 설정

--- src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/favorite/controller/FavoriteController.java
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/favorite/controller/FavoriteController.java
@@ -1,10 +1,21 @@
 package com.beanSpot.WEB3_4_Poten_BE.domain.favorite.controller;
 
+import com.beanSpot.WEB3_4_Poten_BE.domain.favorite.service.FavoriteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * -- 즐겨찾기 컨트롤러 --
  *
  * @author -- 김남우 --
  * @since -- 3월 31일 --
  */
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/favorite")
 public class FavoriteController {
+
+    private final FavoriteService favoriteService;
+
 }
