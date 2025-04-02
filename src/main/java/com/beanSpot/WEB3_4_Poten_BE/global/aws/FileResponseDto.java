a6a6780a20e646e889fbd3355a5392e874a6433d
NH <skwhsmd@naver.com>
2025-04-02 17:39:24 +0900

    S3 파일 업로드 기능 미완성

---
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/global/aws/FileResponseDto.java
@@ -0,0 +1,18 @@
+package com.beanSpot.WEB3_4_Poten_BE.global.aws;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileResponseDto {
+	private String fileName;
+	private String fileUrl;
+	private String fileType;
+	private long fileSize;
+}
