dc76dbfb8d98e8f019721593c7b80f119e2461d6
kk0506014 <kk0506014@gmail.com>
2025-04-03 09:41:50 +0900

    feature: FavoriteCafeRes.java / 주석 추가

--- src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/favorite/dto/FavoriteCafeRes.java
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/favorite/dto/FavoriteCafeRes.java
@@ -2,6 +2,12 @@ package com.beanSpot.WEB3_4_Poten_BE.domain.favorite.dto;
 
 import lombok.Builder;
 
+/**
+ * 즐겨찾기한 카페 정보 반환 DTO
+ *
+ * @author 김남우
+ * @since 2024-04-02
+ */
 @Builder
 public record FavoriteCafeRes(
         Long cafeId,
