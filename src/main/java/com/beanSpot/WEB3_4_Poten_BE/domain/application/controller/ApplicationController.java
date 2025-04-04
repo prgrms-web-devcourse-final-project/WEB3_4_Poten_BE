cc02ab4d03067d65c50a1738e6304a7d1bf57b48
junho3168 <junho3168@naver.com>
2025-04-04 17:44:32 +0900

    feature - 신청 수락 기능 구현

--- src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/application/controller/ApplicationController.java
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/application/controller/ApplicationController.java
@@ -12,10 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.beanSpot.WEB3_4_Poten_BE.domain.application.dto.req.ApplicationReq;
-import com.beanSpot.WEB3_4_Poten_BE.domain.application.dto.res.ApplicationApprovedRes;
 import com.beanSpot.WEB3_4_Poten_BE.domain.application.dto.res.ApplicationRes;
+import com.beanSpot.WEB3_4_Poten_BE.domain.application.entity.Application;
 import com.beanSpot.WEB3_4_Poten_BE.domain.application.service.ApplicationService;
-import com.beanSpot.WEB3_4_Poten_BE.domain.user.repository.UserRepository;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -28,7 +27,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ApplicationController {
 	private final ApplicationService applicationService;
-	private final UserRepository userRepository;
 
 	//인증 방식에 따라서 수정해야합니다.
 	@Operation(
@@ -36,7 +34,7 @@ public class ApplicationController {
 		description = "신청을 추가합니다. 추가 시 신청의 상태 기본값은 PENDING입니다.")
 	@PostMapping
 	public ResponseEntity<ApplicationRes> createApplication(@RequestBody ApplicationReq applicationReq) {
-		ApplicationRes applicationRes = applicationService.createApplication(applicationReq, 1L);
+		ApplicationRes applicationRes = applicationService.createApplication(applicationReq, 2L);
 		return ResponseEntity.ok(applicationRes);
 	}
 
@@ -66,5 +64,15 @@ public class ApplicationController {
 	public ResponseEntity<ApplicationRes> rejectCafe(@PathVariable Long id) {
 		ApplicationRes rejectedCafe = applicationService.rejectCafe(id);
 		return ResponseEntity.ok(rejectedCafe);
+	}
+
+	@Operation(
+		summary = "신청 수락",
+		description = "신청을 APPROVED 상태로 바꿉니다.")
+	@PostMapping("/approve/{id}")
+	public ResponseEntity<ApplicationRes> approveCafe(@PathVariable Long id) {
+		applicationService.approveCafe(id);
+		Application application = applicationService.findById(id);
+		return ResponseEntity.ok(ApplicationRes.fromEntity(application));
 	}
 }
--- src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/application/service/ApplicationService.java
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/application/service/ApplicationService.java
@@ -7,14 +7,11 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 import com.beanSpot.WEB3_4_Poten_BE.domain.application.dto.req.ApplicationReq;
-import com.beanSpot.WEB3_4_Poten_BE.domain.application.dto.res.ApplicationApprovedRes;
 import com.beanSpot.WEB3_4_Poten_BE.domain.application.dto.res.ApplicationRes;
 import com.beanSpot.WEB3_4_Poten_BE.domain.application.entity.Application;
 import com.beanSpot.WEB3_4_Poten_BE.domain.application.entity.Status;
 import com.beanSpot.WEB3_4_Poten_BE.domain.application.exception.ApplicationNotFoundException;
 import com.beanSpot.WEB3_4_Poten_BE.domain.application.repository.ApplicationRepository;
-import com.beanSpot.WEB3_4_Poten_BE.domain.cafe.entity.Cafe;
-import com.beanSpot.WEB3_4_Poten_BE.domain.cafe.repository.CafeRepository;
 import com.beanSpot.WEB3_4_Poten_BE.domain.user.entity.User;
 import com.beanSpot.WEB3_4_Poten_BE.domain.user.exception.UserNotFoundException;
 import com.beanSpot.WEB3_4_Poten_BE.domain.user.repository.UserRepository;
@@ -26,9 +23,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ApplicationService {
 	private final ApplicationRepository applicationRepository;
-	private final CafeRepository cafeRepository;
 	private final UserRepository userRepository;
-	// private final UserRepository userRepository;
 
 	@Transactional
 	public ApplicationRes createApplication(ApplicationReq request, Long userId) {
@@ -78,5 +73,19 @@ public class ApplicationService {
 
 		return ApplicationRes.fromEntity(application);
 	}
-}
 
+	public void approveCafe(Long applicationId) {
+		Application application = applicationRepository.findById(applicationId)
+			.orElseThrow(() -> new ApplicationNotFoundException(applicationId));
+
+		application.approve();
+
+		applicationRepository.save(application);
+	}
+
+	public Application findById(Long id) {
+		return applicationRepository.findById(id)
+			.orElseThrow(() -> new ApplicationNotFoundException(id));
+	}
+
+}
