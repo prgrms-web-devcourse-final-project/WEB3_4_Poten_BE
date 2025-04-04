701b8868bb6cd328ee0f4dff0659488677f25f96
junho3168 <junho3168@naver.com>
2025-04-04 10:36:58 +0900

    refactor - 안쓰는 예외 제거

--- src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/payment/exception/PaymentException.java
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/payment/exception/PaymentException.java
@@ -2,10 +2,6 @@ package com.beanSpot.WEB3_4_Poten_BE.domain.payment.exception;
 
 public class PaymentException extends RuntimeException {
 
-    public PaymentException(String message) {
-        super(message);
-    }
-
     public PaymentException(String message, Throwable cause) {
         super(message, cause);
     }
