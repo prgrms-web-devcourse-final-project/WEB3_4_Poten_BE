000b87c190ea147dafd3157ae44ef3f0837ce69a
kk0506014 <kk0506014@gmail.com>
2025-04-11 10:21:55 +0900

    refactor: 지도 api, 즐겨찾기, 결제 스웨거 설정 추가

--- src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/favorite/controller/FavoriteController.java
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/favorite/controller/FavoriteController.java
@@ -2,6 +2,8 @@ package com.beanSpot.WEB3_4_Poten_BE.domain.favorite.controller;
 
 import com.beanSpot.WEB3_4_Poten_BE.domain.favorite.dto.FavoriteCafeRes;
 import com.beanSpot.WEB3_4_Poten_BE.domain.favorite.service.FavoriteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.List;
  * @author -- 김남우 --
  * @since -- 3월 31일 --
  */
+@Tag(name = "Favorite", description = "Favorite Controller")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/favorite")
@@ -21,18 +24,27 @@ public class FavoriteController {
 
     private final FavoriteService favoriteService;
 
+    @Operation(
+            summary = "카페 즐겨찾기 추가",
+            description = "카페 즐겨찾기 추가")
     @PostMapping("/{memberId}/{cafeId}")
     public ResponseEntity<String> addFavorite(@PathVariable Long memberId, @PathVariable Long cafeId) {
         favoriteService.addFavorite(memberId, cafeId);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(
+            summary = "카페 즐겨찾기 삭제",
+            description = "카페 즐겨찾기 삭제")
     @DeleteMapping("/{memberId}/{cafeId}")
     public ResponseEntity<String> removeFavorite(@PathVariable Long memberId, @PathVariable Long cafeId) {
         favoriteService.removeFavorite(memberId, cafeId);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(
+            summary = "카페 즐겨찾기 조회",
+            description = "즐겨찾기한 카페 조회")
     @GetMapping("/{memberId}")
     public ResponseEntity<List<FavoriteCafeRes>> getFavorites(@PathVariable Long memberId) {
         List<FavoriteCafeRes> favoriteCafes = favoriteService.getFavoriteCafes(memberId);
