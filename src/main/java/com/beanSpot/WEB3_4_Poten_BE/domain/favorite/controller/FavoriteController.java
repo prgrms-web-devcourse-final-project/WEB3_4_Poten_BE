9905cfd7d102341c291d4f97da4921bb332bf4ef
kk0506014 <kk0506014@gmail.com>
2025-04-14 01:29:59 +0900

    refactor: FavoriteController.java / main merge

--- src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/favorite/controller/FavoriteController.java
+++ src/main/java/com/beanSpot/WEB3_4_Poten_BE/domain/favorite/controller/FavoriteController.java
@@ -2,10 +2,12 @@ package com.beanSpot.WEB3_4_Poten_BE.domain.favorite.controller;
 
 import com.beanSpot.WEB3_4_Poten_BE.domain.favorite.dto.FavoriteCafeRes;
 import com.beanSpot.WEB3_4_Poten_BE.domain.favorite.service.FavoriteService;
+import com.beanSpot.WEB3_4_Poten_BE.domain.oauth.SecurityUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,28 +29,35 @@ public class FavoriteController {
     @Operation(
             summary = "카페 즐겨찾기 추가",
             description = "카페 즐겨찾기 추가")
-    @PostMapping("/{memberId}/{cafeId}")
-    public ResponseEntity<String> addFavorite(@PathVariable Long memberId, @PathVariable Long cafeId) {
-        favoriteService.addFavorite(memberId, cafeId);
+    @PostMapping("/{cafeId}")
+    public ResponseEntity<String> addFavorite(
+            @PathVariable Long cafeId,
+            @AuthenticationPrincipal SecurityUser securityUser
+    ) {
+        favoriteService.addFavorite(securityUser.getMember().getId(), cafeId);
         return ResponseEntity.ok().build();
     }
 
     @Operation(
             summary = "카페 즐겨찾기 삭제",
             description = "카페 즐겨찾기 삭제")
-    @DeleteMapping("/{memberId}/{cafeId}")
-    public ResponseEntity<String> removeFavorite(@PathVariable Long memberId, @PathVariable Long cafeId) {
-        favoriteService.removeFavorite(memberId, cafeId);
+    @DeleteMapping("/{cafeId}")
+    public ResponseEntity<String> removeFavorite(
+            @PathVariable Long cafeId,
+            @AuthenticationPrincipal SecurityUser securityUser
+    ) {
+        favoriteService.removeFavorite(securityUser.getMember().getId(), cafeId);
         return ResponseEntity.ok().build();
     }
 
     @Operation(
             summary = "카페 즐겨찾기 조회",
             description = "즐겨찾기한 카페 조회")
-    @GetMapping("/{memberId}")
-    public ResponseEntity<List<FavoriteCafeRes>> getFavorites(@PathVariable Long memberId) {
-        List<FavoriteCafeRes> favoriteCafes = favoriteService.getFavoriteCafes(memberId);
+    @GetMapping
+    public ResponseEntity<List<FavoriteCafeRes>> getFavorites(
+            @AuthenticationPrincipal SecurityUser securityUser
+    ) {
+        List<FavoriteCafeRes> favoriteCafes = favoriteService.getFavoriteCafes(securityUser.getMember().getId());
         return ResponseEntity.ok(favoriteCafes);
     }
-
 }
