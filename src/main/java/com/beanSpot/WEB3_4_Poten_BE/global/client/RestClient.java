086c0de7ac97304cd2756437bb0a64e6430c7c3c
kk0506014 <kk0506014@gmail.com>
2025-04-14 15:29:36 +0900

    refactor: 추상화 롤백

--- src/main/java/com/beanSpot/WEB3_4_Poten_BE/global/client/RestClient.java
+++
@@ -1,9 +0,0 @@
-package com.beanSpot.WEB3_4_Poten_BE.global.client;
-
-import org.springframework.http.ResponseEntity;
-
-import java.util.Map;
-
-public interface RestClient {
-    ResponseEntity<Map> get(String uri, String authorizationHeader);
-}
